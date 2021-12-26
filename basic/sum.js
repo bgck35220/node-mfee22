6dc38827ebd93c2bad35082237ef00878f99c017
Miru <hungche0701@gmail.com>
2021-12-26 21:43:59 +0800

    修改壓力測試

--- basic/sum.js
+++ basic/sum.js
@@ -39,36 +39,24 @@ function sum(n) {
 // console.log(sum(10));
 
 //梯形寫法
-// function sum(n) {
-//     let result = (1 + n) * n / 2
-//     return result
-// }
+function sum1(n) {
+    let result = (1 + n) * n / 2
+    return result
+}
 // console.log(sum(1));
 // console.log(sum(2));
 // console.log(sum(5));
 // console.log(sum(10));
 
 //壓力測試
-console.time('recursive');
-for (let i = 0; i < 10000; i++)
-    // sum(100000); // maximum call stack size exceeded
-    // sum(50000); // maximum call stack size exceeded
-    // sum(10000); // 1.198 秒
-    sum(100); // 62.087 毫秒
-console.timeEnd('recursive');
+console.time("for");
+for (let i = 1; i <= 10000; i++) {
+  sum(100000); // O(n)
+}
+console.timeEnd("for");
 
-console.time('for')
-for (let i = 0; i < 10000; i++)
-    // sum_for(100000); // 1.194 秒
-    // sum_for(50000); // 510.471 毫秒
-    // sum_for(10000); // 112.668 毫秒
-    sum_for(100); // 4.485 毫秒
-console.timeEnd('for')
-
-console.time('trapezoid')
-for (let i = 0; i < 10000; i++)
-    // sum_trapezoid(100000); // 2.305 毫秒
-    // sum_trapezoid(50000); // 1.04 毫秒
-    // sum_trapezoid(10000); // 0.691 毫秒
-    sum_trapezoid(100); // 1.155 毫秒
-console.timeEnd('trapezoid')
+console.time("formula");
+for (let i = 1; i <= 10000; i++) {
+  sum1(100000); // O(1)
+}
+console.timeEnd("formula");
