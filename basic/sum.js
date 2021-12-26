78e06d02352bc4f8d2c2809cb60561ab98189449
Miru <hungche0701@gmail.com>
2021-12-26 14:39:22 +0800

    增加遞迴寫法

--- basic/sum.js
+++ basic/sum.js
@@ -1,3 +1,4 @@
+//迴圈
 function sum(n) {
     let result = 0;
     for (let i = 1; i <= n; i++) {
@@ -9,3 +10,15 @@ console.log(sum(1));
 console.log(sum(2));
 console.log(sum(5));
 console.log(sum(10));
+
+// 遞迴
+function sum(n) {
+    if (n < 1)
+        return 0;
+    return n + sum(n - 1);
+}
+
+console.log('sum(1) :>> ', sum(1));
+console.log('sum(2) :>> ', sum(2));
+console.log('sum(5) :>> ', sum(5));
+console.log('sum(10) :>> ', sum(10));
