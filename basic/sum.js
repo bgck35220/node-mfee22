2d211aa6ef8071f3c2bb575b0cbada6b96e3a3fd
Miru <hungche0701@gmail.com>
2021-12-26 14:55:46 +0800

    增加reducer、梯形寫法

--- basic/sum.js
+++ basic/sum.js
@@ -1,4 +1,5 @@
-//迴圈
+//for迴圈寫法
+
 function sum(n) {
     let result = 0;
     for (let i = 1; i <= n; i++) {
@@ -6,19 +7,50 @@ function sum(n) {
     }
     return result;
 }
-console.log(sum(1));
-console.log(sum(2));
-console.log(sum(5));
-console.log(sum(10));
 
-// 遞迴
-function sum(n) {
-    if (n < 1)
-        return 0;
-    return n + sum(n - 1);
-}
+// console.log(sum(1));
+// console.log(sum(2));
+// console.log(sum(5));
+// console.log(sum(10));
 
-console.log('sum(1) :>> ', sum(1));
-console.log('sum(2) :>> ', sum(2));
-console.log('sum(5) :>> ', sum(5));
-console.log('sum(10) :>> ', sum(10));
+// 遞迴寫法
+
+// function sum(n) {
+//     if (n < 1)
+//         return 0;
+//     return n + sum(n - 1);
+// }
+
+// console.log('sum(1) :>> ', sum(1));
+// console.log('sum(2) :>> ', sum(2));
+// console.log('sum(5) :>> ', sum(5));
+// console.log('sum(10) :>> ', sum(10));
+
+//reducer寫法
+
+// const sum = (n) => {
+//     arr = [];
+//     const reducer = (previousValue, currentValue) => previousValue + currentValue;
+//     for (i = 1; i <= n; i++) {
+//         arr.push(i);
+//     }
+//     let result = arr.reduce(reducer, 0);
+//     return result;
+// };
+
+// console.log(sum(1));
+// console.log(sum(2));
+// console.log(sum(5));
+// console.log(sum(10));
+
+//梯形寫法
+
+// function sum(n) {
+//     let result = (1 + n) * n / 2
+//     return result
+// }
+
+// console.log(sum(1));
+// console.log(sum(2));
+// console.log(sum(5));
+// console.log(sum(10));
