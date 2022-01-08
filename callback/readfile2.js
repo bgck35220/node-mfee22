9df1ca1f37e6a286eea4e21fd84ef13eb2bd15a9
Miru <hungche0701@gmail.com>
2022-01-08 22:37:06 +0800

    上傳readfile2.js

--- callback/readfile2.js
+++ callback/readfile2.js
@@ -1,10 +1,22 @@
-const { readFile } = require("fs");
+// Promise 物件代表一個即將完成、或失敗的非同步操作，以及它所產生的值
 
-readFile("test.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-  // insert to mysql
-})
+const { readFile } = require("fs");
+let readfilePromise = new Promise((resolve, reject) => {
+  readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(data);
+    // insert to myasql
+  });
+});
+console.log(readfilePromise);
+readfilePromise
+  .then((result) => {
+    //接住resolve的結果
+    console.log(`這是 Promise 的 result: ${result}`)
+  })
+  .catch((err) => {
+    console.log(`這是 Promise 的 err: ${err}`)
+  })
