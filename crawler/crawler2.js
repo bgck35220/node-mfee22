7169f23dbb8e7acc2c6166b9bf9308e8e149b85b
Miru <hungche0701@gmail.com>
2022-01-14 21:30:48 +0800

    add crawler2.js

---
+++ crawler/crawler2.js
@@ -0,0 +1,30 @@
+// 用 async/await 寫法練習 axios.get 去證交所取得資料
+
+const axios = require("axios"); //引入套件
+
+(async () => {
+  try {
+    let response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20211201&stockNo=2330&_=1641716312720"
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+// https://blog.csdn.net/candyHZhou/article/details/105240672
+
+// async 函數處理多個異步函數
+// axios.defaults.baseURL =
+//   "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20211201&stockNo=2330&_=1641716312720";
+// async function queryData() {
+//   // # 2.1 添加await之後 當前的await 返回結果之後才會執行後面的代碼
+//   let info = await axios.get("async1");
+//   // #2.2 讓異步代碼看起來、表現起來更像同步代碼
+//   let ret = await axios.get("async2?info=" + info.data);
+//   return ret.data;
+// }
+// queryData().then(function (data) {
+//   console.log(data.data);
+// });
