cb9758bb3140d4f824afa50524d53dbbae930650
Miru <hungche0701@gmail.com>
2022-01-15 13:59:31 +0800

    增加自己寫date寫法

--- crawler/crawler3-data.js
+++ crawler/crawler3-data.js
@@ -2,6 +2,34 @@ const axios = require('axios');
 const { readFile } = require('fs/promises');
 const moment = require('moment');
 
+// 自己寫date
+
+// (async () => {
+//   try {
+//     let standardDate = new Date();
+//     let year = standardDate.getFullYear().toString();
+//     let month = standardDate.getMonth() + 1;
+//     if (month < 10) {
+//       month = '0' + month;
+//     }
+//     let date = standardDate.getDate().toString();
+//     let searchDate = `${year}${month}${date}`;
+//     let stockNum = await readFile('./stock.txt', 'utf-8');
+//     let source = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+//       params: {
+//         response: 'json',
+//         date: searchDate,
+//         stockNo: stockNum,
+//       },
+//     });
+//     console.log(source.data);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })();
+
+// 使用moment
+
 (async () => {
   try {
     // 根據變數去抓取資料
