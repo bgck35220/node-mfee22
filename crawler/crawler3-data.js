9b98674266e54c2a062ff7aea4a22495d0e6abbd
Miru <hungche0701@gmail.com>
2022-01-15 13:20:12 +0800

    新增now方法

--- crawler/crawler3-data.js
+++ crawler/crawler3-data.js
@@ -8,6 +8,7 @@ const moment = require('moment');
     // 從 stock.txt 中讀出檔案代碼
     let stockNo = await readFile('stock.txt', 'utf-8');
     let queryDate = moment().format('YYYYMMDD'); // 自動用今天的日期
+    // let queryDate = 'now'; // 可以用now抓當天日期
 
     let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
       // 這裡可以放一些設定
