c6d13b37da69bc25c21ad140ccc41f587ad20cf5
Miru <hungche0701@gmail.com>
2022-01-21 16:24:37 +0800

    更新

--- simple-web/server.js
+++ simple-web/server.js
@@ -1,10 +1,31 @@
-const http = require('http');
-require('dotenv').config();
+// nodejs 內建的模組，不用安裝
+const http = require("http");
+require("dotenv").config();
+
 const server = http.createServer(function (request, response) {
-  response.end('Hello,Server');
+  // TODO: 怎麼處理 request, 要負責回覆 response
+  // status code
+  // 1xx, 2xx, 3xx, 4xx, 5xx
+
+  response.statusCode = 200;
+  const path = request.url;
+  console.log(path);
+
+  switch (path) {
+    case "/":
+      response.end("Hello, Server EFG");
+      break;
+    case "/about":
+      response.end("Hello, this is about");
+      break;
+    default:
+      response.statusCode = 404;
+      response.end();
+  }
 });
 
-let port = process.env.SERCER_PORT || 3000;
+// 做預設值
+let port = process.env.SERVER_PORT || 3000; //給預設值 如果.env設定檔的port是undifined或null 就執行後面的開在3000
 server.listen(port, () => {
-  console.log(`我們簡易版server ${port}`);
+  console.log(`我們的簡易版 server 已經啟動，在 port ${port} 上`);
 });
